e781c28c7bd58cdddca964fa7bed0cb0a7191086
Nilton da Silva Junior <tonsti@gmail.com>
2020-11-09 13:09:14 +0100

    Project created

---
+++ backend/src/main/java/com/nilton/vocabularyweb/VocabularywebApplication.java
@@ -0,0 +1,13 @@
+package com.nilton.vocabularyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularywebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nilton/vocabularyweb/VocabularywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilton.vocabularyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
